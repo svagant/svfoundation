9950609686dec0d5d26c5aec5228707825d2441d
Stanislav Smida <s@svagant.com>
2018-07-08 17:31:11 +0200

    Initial project

---
+++ SVFoundation/SVFoundation/SVFoundation.h
@@ -0,0 +1,19 @@
+//
+//  SVFoundation.h
+//  SVFoundation
+//
+//  Created by Stanislav Smida on 08/07/2018.
+//  Copyright © 2018 Svagant, s.r.o. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SVFoundation.
+FOUNDATION_EXPORT double SVFoundationVersionNumber;
+
+//! Project version string for SVFoundation.
+FOUNDATION_EXPORT const unsigned char SVFoundationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SVFoundation/PublicHeader.h>
+
+
